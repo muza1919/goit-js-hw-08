775fac1677f7bd9bd1dc0d3ba97fbf4c23ddc2b7
muza1919 <109676246+muza1919@users.noreply.github.com>
2022-12-29 19:08:26 +0000

    Deploying to gh-pages from @ muza1919/goit-js-hw-08@94c3071c0df89b3ac2fe83742bedd92a60c1a52b 🚀

--- 02-video.be9280e3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Player(e),o="video-current-time";t.on("timeupdate",throttle((function(e){localStorage.setItem(o,e.seconds)}),1e3)),t.on("play",(function(){var e=localStorage.getItem(o);e&&t.setCurrentTime(e)}))}();
-//# sourceMappingURL=02-video.be9280e3.js.map
