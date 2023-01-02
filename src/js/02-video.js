cef465353a441a8dd901547b6a502ebd277a2aeb
Alona <alena2003305@gmail.com>
2023-01-02 14:19:13 +0200

    bug fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,16 +5,20 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = 'video-current-time';
 
+
 player.on('timeupdate', throttle(findCurrentTime, 1000))
 
 function findCurrentTime(event){
     localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
 }
  
-player.on('play', () => {
-      const newTime = localStorage.getItem(LOCALSTORAGE_KEY);
-      if (newTime) {
-        player.setCurrentTime(newTime)
-      }
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
     }
-  );
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,41 @@
 import throttle from 'lodash.throttle';
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-const form = document.querySelector('.feedback-form')
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
 addText();
 
-const formData = {};
+const formData = {
+    [email.name] : email.value,
+    [message.name]: message.value,
+};
+
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event){
-    formData[event.target.name] = event.target.value;
+    formData[email.name] = email.value;
+    formData[message.name] = message.value;
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 };
 
 function onFormSubmit(event){
     event.preventDefault();
     event.currentTarget.reset();
-    console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(formData);
     localStorage.removeItem(LOCALSTORAGE_KEY)
 }
 
 function addText() {
     const data = localStorage.getItem(LOCALSTORAGE_KEY);
-    if (!data) {
+    const valuesText = JSON.parse(data);
+    if (!valuesText ) {
         return;
     }
 
-    const value = JSON.parse(data);
-
-    if(value.email){
-     form.email.value = value.email;
-    } else { 
-        form.email.value = '' 
-    }
-
-    if (value.message) {
-           form.message.value = value.message;
-    } else {
-           form.message.value = '';
-    }
-}
+    email.value = valuesText.email;
+    message.value = valuesText.message;  
+ }
